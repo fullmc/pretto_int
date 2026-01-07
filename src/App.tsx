f1d47c6f47799c1f74b94bb353cdf15547d6757b
MC <marie-claire.lambo@edu.devinci.fr>
2026-01-07 11:52:50 +0100

    ✨ feat(phone-header): add fonts, change title, add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import cellular from './assets/cellular.svg'
+import wifi from './assets/Wifi.svg'
+import battery from './assets/Battery.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="app-header">
+        <div className="time">9:41</div>
+        <div className="status-icons">
+          <img src={cellular} alt="Cellular signal" />
+          <img src={wifi} alt="WiFi signal" />
+          <img src={battery} alt="Battery level" />
+        </div>
+      </header>
     </>
   )
 }
