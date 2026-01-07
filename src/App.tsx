878219e12d6df174d8d7a9cc5c872badc43b20db
MC <marie-claire.lambo@edu.devinci.fr>
2026-01-07 12:45:58 +0100

    ✨ feat(header): update header and status bar layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import './App.css'
 import cellular from './assets/cellular.svg'
 import wifi from './assets/Wifi.svg'
 import battery from './assets/Battery.svg'
+import logo from './assets/Logo_dark.svg'
+import menu from './assets/Bars.svg'
 
 function App() {
 
   return (
     <>
-      <header className="app-header">
-        <div className="time">9:41</div>
-        <div className="status-icons">
-          <img src={cellular} alt="Cellular signal" />
-          <img src={wifi} alt="WiFi signal" />
-          <img src={battery} alt="Battery level" />
+      <header>
+        <div className="status-bar">
+          <div className="time">9:41</div>
+          <div className="status-icons">
+            <img src={cellular} alt="Cellular signal" />
+            <img src={wifi} alt="WiFi signal" />
+            <img src={battery} alt="Battery level" />
+          </div>
         </div>
+
+        <nav>
+          <img src={logo} alt="Pretto Logo" />
+          <div className="menu">
+            <img src={menu} alt="Menu" />
+          </div>
+        </nav>
       </header>
     </>
   )
