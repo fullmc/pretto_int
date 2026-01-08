e6bcc148ad789b8a844d23cabebf9c53f64a4eac
MC <marie-claire.lambo@edu.devinci.fr>
2026-01-08 13:53:09 +0100

    ✨ feat(alert-settings): add styles and structure

--- src/AlertSettings.tsx
+++ src/AlertSettings.tsx
@@ -1,9 +1,33 @@
+import Header from "./components/Header";
+import './AlertSettings.css'
+
 function AlertSettings() {
   return (
     <>
-      <main>
-        <section>
+      <Header />
+      <main className="alert-page">
+        <section className="alert-form">
           <h1>Paramétrez votre alerte</h1>
+          
+          <div className="form-content">
+            <div className="form-section">
+              <h5>Localisation</h5>
+            </div>
+            <div className="form-section">
+              <h5>Type de bien</h5>
+            </div>
+            <div className="form-section">
+              <h5>Type de vente</h5>
+            </div>
+          </div>
+          <div className="form-buttons">
+            <button className="save-btn">
+              Enregistrer
+            </button>
+            <button className="delete-btn">
+              Supprimer l'alerte
+            </button>
+          </div>
         </section>
       </main>
     </>
